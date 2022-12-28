020e3cafd25f8911a12b0048a91ef085dc448dfe
Daniel Ancines <daniel.ancines@gmail.com>
2022-12-28 09:26:17 -0300

    Remove project

--- Maui/src/Maui.Core/Platforms/Android/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Maui.Core;
-
-// All the code in this file is only included on Android.
-public class PlatformClass1
-{
-}
-
--- Maui/src/Maui.Core/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Maui.Core;
-
-// All the code in this file is only included on iOS.
-public class PlatformClass1
-{
-}
-
