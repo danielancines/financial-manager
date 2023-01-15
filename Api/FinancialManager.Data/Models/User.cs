a8cd84e1929ff66f20b4f5e0386629f36fce198f
Daniel Ancines <daniel.ancines@gmail.com>
2023-01-15 16:07:35 -0300

    Project folders structures adjustments.

---
+++ Api/FinancialManager.Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FinancialManager.Data.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Roles { get; set; }
+    public bool Active { get; set; }
+}
