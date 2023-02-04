97eeabd0d62c2be04e2e61dbfac166baa946fe61
Daniel Ancines <daniel.ancines@gmail.com>
2023-02-04 17:44:27 -0300

    UI customization, change icons e visual updates

--- Maui/Maui.FinancialManager/MauiProgram.cs
+++ Maui/Maui.FinancialManager/MauiProgram.cs
@@ -19,7 +19,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("SF-Pro-Rounded-Regular.otf", "SFProRounded");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
