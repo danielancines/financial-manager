0c4341e0a558ce5e45f3a2700456975e8cf317ba
Daniel Ancines <daniel.ancines@gmail.com>
2023-02-04 21:05:52 -0300

    Fix #22 - Error at search for medicines prices

--- Maui/Maui.FinancialManager/Searchers/DrogaRaiaSearcher.cs
+++ Maui/Maui.FinancialManager/Searchers/DrogaRaiaSearcher.cs
@@ -22,9 +22,12 @@ public class DrogaRaiaSearcher : IMedicineSearcher
     public async Task<IEnumerable<Medicine>> SearchAsync(string searchTerm)
     {
         var httpClient = new HttpClient();
-        httpClient.DefaultRequestHeaders.Add("x-api-key", "5f308895c59fadb0b9ed43341c6eb33e41e78394d3ca970c5a285e91d25bc9cd");
+        httpClient.DefaultRequestHeaders.Add("x-api-key",
+            "5f308895c59fadb0b9ed43341c6eb33e41e78394d3ca970c5a285e91d25bc9cd");
 
-        var content = new StringContent("{\"query\": \"{search(search: {term:\\\"Benicar\\\",searchApiVersion:LINX}, isStixNewAccelerator: true) {products {name, image, gallery, packageQty, description, availability {hasStock},oldPrice{value}, price {value}}}}\"}".Replace("!searchTerm!", searchTerm), Encoding.UTF8, "application/json");
+        var content = new StringContent(
+            "{\"query\": \"{search(search: {term:\\\"!searchTerm!\\\",searchApiVersion:LINX}, isStixNewAccelerator: true) {products {name, image, gallery, packageQty, description, availability {hasStock},oldPrice{value}, price {value}}}}\"}"
+                .Replace("!searchTerm!", searchTerm), Encoding.UTF8, "application/json");
 
         var response = await httpClient.PostAsync(URL, content);
 
