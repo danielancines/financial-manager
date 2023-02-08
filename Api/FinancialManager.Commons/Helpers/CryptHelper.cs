edc4b1543ebfd43c99dd7bcbd6597671a62eea1d
Daniel Ancines <daniel.ancines@gmail.com>
2023-02-08 08:34:48 -0300

    Close #7 - Removed sensitive info

--- Api/FinancialManager.Commons/Helpers/CryptHelper.cs
+++ Api/FinancialManager.Commons/Helpers/CryptHelper.cs
@@ -5,8 +5,9 @@ namespace FinancialManager.Commons.Helpers;
 
 public static class CryptHelper
 {
+    public static string CryptEncodingKey { get; set; }
     public static string EncryptString(string str)
     {
-        return Convert.ToBase64String(KeyDerivation.Pbkdf2(str, Encoding.ASCII.GetBytes("LOvyTCu1XCIkHE / TfIuIHA =="), KeyDerivationPrf.HMACSHA256, iterationCount: 100000, numBytesRequested: 256 / 8));
+        return Convert.ToBase64String(KeyDerivation.Pbkdf2(str, Encoding.ASCII.GetBytes(CryptEncodingKey), KeyDerivationPrf.HMACSHA256, iterationCount: 100000, numBytesRequested: 256 / 8));
     }
 }
